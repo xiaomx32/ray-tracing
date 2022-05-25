066617c2e932a6e4c537c04bec42e9fcfa2fe3ac
xiaomx32 <2116906041@qq.com>
2022-05-25 10:02:39 +0800

    a little modify

--- main.cpp
+++ main.cpp
@@ -1,22 +1,8 @@
 #include <iostream>
 
 int main() {
-    //int nx = 200;
-    //int ny = 100;
-
-    //std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    //for (int j = ny - 1; j >= 0; --j) {
-    //    for (int i = 0; i < nx; ++i) {
-    //        float r = float(i) / float(nx);
-    //        float g = float(j) / float(ny);
-    //        float b = 0.2;
-    //        int ir = int(255.99 * r);
-    //        int ig = int(255.99 * g);
-    //        int ib = int(255.99 * b);
-    //        std::cout << ir << " " << ig << " " << ib << "\n";
-    //    }
-    //}
     std::cout << "Hello ray tracing!";
-    system("puase");
+    std::cin.get();
+    
     return 0;
 }
