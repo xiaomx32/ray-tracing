fe00ca70009d397c4650e47784ce1a20793f2851
xiaomx32 <2116906041@qq.com>
2022-05-27 23:03:08 +0800

    update

--- main.cpp
+++ main.cpp
@@ -44,12 +44,22 @@ int main() {
     return 0;
 }
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 /*
- * ray_color(ray) 函数根据 y 值将蓝白做线性插值的混合
- * 对射线的方向进行单位化, 以保证 y 的取值范围（-1.0 < y < 1.0）
- * 因为我们使用 y 轴做渐变, 所以你可以看到这个蓝白渐变也是竖直的
+* 使用红色的线表示射线击中（0, 0, -1）处的小球
 */
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
