553b5b5f9ceb8b3cf7ce2e9e1b60d3a0c253fa89
xiaomx32 <2116906041@qq.com>
2022-06-01 21:19:31 +0800

    add a new picture

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 
+double hit_sphere(const point3& center, double radius, const ray& r);
 color ray_color(const ray& r, const hittable& world);
 
 int main() {
@@ -14,7 +15,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 1000;
 
     // World
     hittable_list world;
@@ -31,7 +32,7 @@ int main() {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
+            for (int s = 0; s < samples_per_pixel; ++s) {// 此循环，用于对每个像素进行多重采样
                 auto u = (i + random_double()) / (double)(image_width - 1);
                 auto v = (j + random_double()) / (double)(image_height - 1);
                 ray r = cam.get_ray(u, v);
--- rtweekend.h
+++ rtweekend.h
@@ -15,16 +15,13 @@
 #include <memory>
 #include <cstdlib>// Ëæ»úÊý
 
-// Usings
 using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
 
-// Constants
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-// Utility Functions
 double degrees_to_radians(double degrees);
 
 double random_double();
