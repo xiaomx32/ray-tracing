9e9e1d39bcdb14440d3dbb23b7c1c32a664b13b5
xiaomx32 <2116906041@qq.com>
2022-06-04 00:37:12 +0800

    update

--- main.cpp
+++ main.cpp
@@ -17,10 +17,10 @@ color ray_color(const ray& r, const hittable& world, int depth);
 
 int main() {
     // image
-    const auto aspect_ratio = 3.0 / 2.0;
+    const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 500;
+    const int samples_per_pixel = 5;
     const int max_depth = 50;
 
     // World
