a18d4c5d50cbed09a5c89a03ed4b4fee9c193f19
xiaomx32 <2116906041@qq.com>
2022-06-21 10:39:12 +0800

    delete some files

--- color.cpp
+++
@@ -1,8 +0,0 @@
-#include "color.h"
-
-void write_color(std::ostream& out, color pixel_color) {
-	// write the translated [0, 255] value of each color component.
-	out << static_cast<int>(255.999 * pixel_color.x()) << " "
-		<< static_cast<int>(255.999 * pixel_color.y()) << " "
-		<< static_cast<int>(255.999 * pixel_color.z()) << "\n";
-}
--- rtweekend.cpp
+++
@@ -1,6 +0,0 @@
-#include "rtweekend.h"
-
-// Utility Functions
-double degrees_to_radians(double degrees) {
-    return degrees * pi / 180.0;
-}
