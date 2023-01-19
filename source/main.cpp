f6304acf3bf10902d756da3b8bbfe18036d0fa75
xiaomx32 <2116906041@qq.com>
2023-01-19 23:31:51 +0800

    update

--- source/main.cpp
+++ source/main.cpp
@@ -5,31 +5,24 @@
 
 #include <iostream>
 
-double hit_sphere(const point3& center, double radius, const ray& r);
+//double hit_sphere(const point3& center, double radius, const ray& r);
 color ray_color(const ray& r, const hittable& world);
 
 int main() {
-
     // image
-    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_height = 100;
     const int image_width = 400;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // World
     hittable_list world;
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    // 在（0, -100.5, -1）处，放置一个球
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));// 在（0, -100.5, -1）处，放置一个球
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
 
     // Render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
@@ -49,30 +42,6 @@ int main() {
     std::cin.get();
 
     return 0;
-}
-
-// 计算球体表面与射线的交点以及该点的法线
-// 直接将法相值作为颜色输出
-// 只需要计算离射线原点最近的那个交点的法相就行了, 后面的东西会被遮挡
-double hit_sphere(const point3& center, double radius, const ray& r) {// 直线与球的交点
-    vec3 oc = r.origin() - center;// 射线的起点、球心
-    //auto a = dot(r.direction(), r.direction());
-    //auto b = 2.0 * dot(oc, r.direction());
-    //auto c = dot(oc, oc) - radius * radius;
-    //auto discriminant = b * b - 4 * a * c;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius * radius;
-    auto discriminant = half_b * half_b - a * c;
-
-    if (discriminant < 0.0) {
-        return -1.0;
-    }
-    else {
-        return (-half_b - sqrt(discriminant)) / a;
-    }
-
-    return 0.0;
 }
 
 // 使用红色的线表示射线击中（0, 0, -1）处的小球
