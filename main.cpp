25871384ec16f9655ee942738ffaf09f8efa4842
xiaomx32 <2116906041@qq.com>
2023-01-20 00:19:13 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 
 int main() {
-    int nx = 200;
-    int ny = 100;
+    const int image_width = 200;
+    const int image_height = 100;
 
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    for (int j = ny - 1; j >= 0; --j) {
-        for (int i = 0; i < nx; ++i) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
+    std::cout << "P3\n"
+              << image_width << " " << image_height << "\n255\n";
+    
+    for (int j = image_height - 1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+            float r = float(i) / float(image_width);
+            float g = float(j) / float(image_height);
             float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            int ir = static_cast<int>(255.99 * r);
+            int ig = static_cast<int>(255.99 * g);
+            int ib = static_cast<int>(255.99 * b);
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
+
+    std::cin.get();
     
     return 0;
 }
