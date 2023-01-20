e06c1230c58249228584b0d3743ec8134efd3271
xiaomx32 <2116906041@qq.com>
2023-01-20 13:28:08 +0800

    update

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include <rtweekend\rtweekend.h>
+#include <color\color.h>
 #include <hittable\hittable_list.h>
 #include <sphere\sphere.h>
 
@@ -8,8 +9,9 @@ color ray_color(const ray& r, const hittable& world);
 
 int main() {
     // image
-    const int image_width = 200;
-    const int image_height = 100;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // World
     hittable_list world;
@@ -17,10 +19,14 @@ int main() {
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));// 在（0, -100.5, -1）处，放置一个球
 
     // Camera
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     // Render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
