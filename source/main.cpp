e6eb307d45328846dbd622aeca7349cf9613451c
xiaomx32 <2116906041@qq.com>
2023-01-20 13:14:14 +0800

    update

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,38 @@
 #include <vec3/vec3.h>
+#include <ray/ray.h>
+#include <color/color.h>
 #include <iostream>
 
+color ray_color(const ray& r);
 
 int main() {
+    
     // image
-    const int image_width = 200;
-    const int image_height = 100;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    
     // Render
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            vec3 color(double(i)/image_width, double(j)/image_height, 0.2);
-            color.write_color(std::cout);
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
         }
     }
 
